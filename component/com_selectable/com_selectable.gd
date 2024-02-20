f7ad595d60ac7ef13f334c458ae915c4edd1c2a3
natsuki <yasashisainternet@gmail.com>
2024-02-20 14:49:40 +0900

    [add] : com_selectableを追加
    オブジェクトを選択可能にするコンポーネントを作成しました。

---
+++ component/com_selectable/com_selectable.gd
@@ -0,0 +1,23 @@
+extends Area2D
+class_name com_selectable
+
+var is_selected: bool = false;
+var _material: ShaderMaterial;
+@export var sprite_2d: Sprite2D;
+
+func _init():
+	_material = ShaderMaterial.new()
+	_material.shader = load("res://component/com_selectable/highlight.gdshader")
+
+func _ready():
+	if sprite_2d != null:
+		sprite_2d.material = _material
+		_material.set_shader_parameter("highlight", 0)
+
+func _on_mouse_entered():
+	is_selected = true;
+	_material.set_shader_parameter("highlight", 0.5)
+
+func _on_mouse_exited():
+	is_selected = false;
+	_material.set_shader_parameter("highlight", 0)
