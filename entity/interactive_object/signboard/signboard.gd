a96229d7fc0b678a7abf4b968510e569252e57d8
natsuki <yasashisainternet@gmail.com>
2024-02-20 14:50:57 +0900

    [update] : 看板をクリックでインタラクト可能なオブジェクトに変更しました。

--- entity/interactive_object/signboard/signboard.gd
+++ entity/interactive_object/signboard/signboard.gd
@@ -1,13 +1,4 @@
 extends StaticBody2D
 
-@onready var interaction_area: InteractionArea = $InteractionArea
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	interaction_area.interact = Callable(self, "_on_interact")
-	
-
-func _on_interact():
-	print("看板には何も書かれていない。")
+func _on_com_interactable_interacted():
 	TextboxManager.main_textbox("看板にはなにも書かれていない。")
