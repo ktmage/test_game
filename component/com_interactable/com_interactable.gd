0de68881ea50610363f061666b2770443b9095dc
natsuki <yasashisainternet@gmail.com>
2024-02-20 14:50:25 +0900

    [add] : com_interactableを追加
    オブジェクトをインタラクト可能にするコンポーネントを追加しました。

---
+++ component/com_interactable/com_interactable.gd
@@ -0,0 +1,17 @@
+extends Area2D
+class_name com_interactable
+
+@export var com_selectable: com_selectable;
+var is_entered: bool = false
+
+signal interacted;
+
+func _on_body_entered(body):
+	is_entered = true
+
+func _on_body_exited(body):
+	is_entered = false
+
+func _input(event):
+	if event.is_action_pressed("interact") and is_entered and com_selectable.is_selected:
+		emit_signal("interacted")
