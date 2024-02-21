bed1e37aee642e72b1f5d82c739d858d1ef70523
forestgunp-man <145314122+forestgunp-man@users.noreply.github.com>
2024-02-21 19:02:04 +0900

    [update] : ステータスコンポーネントの試作
    ステータスコンポーネントを試作したけど、別に今作るもんでもなかった気がする

---
+++ component/com_status/com_status.gd
@@ -0,0 +1,14 @@
+extends Node
+class_name com_status
+
+@export_category("basic")
+@export var hp: int
+@export var mp: int
+
+@export var speed: int
+
+@export var atk: int
+@export var mtk:int
+
+@export var def: int
+@export var  mdef: int
