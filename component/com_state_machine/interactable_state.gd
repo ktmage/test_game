5ff365f9b923eaeefc1ceb17ba77587215d49991
natsuki <yasashisainternet@gmail.com>
2024-02-23 16:36:20 +0900

    [add] : interactable_state/non_interactable_stateを追加
    インタラクト可能な状態とインタラクト不可能な状態を追加しました。

---
+++ component/com_state_machine/interactable_state.gd
@@ -0,0 +1,25 @@
+extends State
+class_name InteractableState
+
+@export var com_interact_area: com_interact_area;
+@export var com_select_area: com_select_area;
+
+signal interacted
+
+func _enter():
+	pass
+	
+func _update(delta):
+	pass
+	
+func _physics_update(delta):
+	pass
+	
+func _exit():
+	pass 
+	
+func _input_update(event):
+	if event.is_action_pressed("interact") and com_interact_area.is_entered and com_select_area.is_selected :
+		interacted.emit()
+		transitioned.emit(self, "NonInteractableState")
+
