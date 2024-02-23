a83a3d2daee08782dee988a78b67d0586c0cc32a
natsuki <yasashisainternet@gmail.com>
2024-02-23 15:43:23 +0900

    [update] : state_machineの修正
    名前を変更、シーンを削除。

---
+++ component/com_state_machine/com_state_machine.gd
@@ -0,0 +1,49 @@
+extends Node
+class_name com_state_machine
+## StateMachineコンポーネント
+##
+## Stateという単位でオブジェクトの状態を表現するために、それぞれの状態の遷移を管理するコンポーネント。
+
+## 初期化用のStateを保持するプロパティ
+@export var initial_state : State
+
+## 現在のStateを保持するプロパティ
+var current_state : State
+
+## ステートマシンの子に配置されたStateを保持するプロパティ
+var states : Dictionary = {}
+
+func _ready():
+	assert(initial_state, "Initial state is not set.")
+	if initial_state:
+		initial_state._enter()
+		current_state = initial_state
+
+	for child in get_children():
+		assert(child is State, "StateMachine can only have State as children.")
+		if child is State:
+			states[child.name] = child
+			child.transitioned.connect(_on_transitioned)
+
+func _process(delta):
+	if current_state:
+		current_state._update(delta)
+
+func _physics_process(delta):
+	if current_state:
+		current_state._physics_update(delta)
+
+func _on_transitioned(state : State, new_state_name : String) -> void :
+	if state != current_state:
+		return;
+		
+	var new_state = states.get(new_state_name)
+	if !new_state:
+		return;
+		
+	if current_state:
+		current_state._exit()
+		
+	new_state._enter()
+		
+	current_state = new_state;
