2ec384ee0496b579edb6f4a77e1b5d0687e940e2
natsuki <yasashisainternet@gmail.com>
2024-02-24 23:37:26 +0900

    [update] : state_template.gdを修正
    transitionedの宣言を追加。それぞれのコメントを追加。

--- script_templates/State/state_template.gd
+++ script_templates/State/state_template.gd
@@ -1,17 +1,25 @@
 extends State
 #class_name 
 
+# 遷移したいときにemitするシグナル
+signal transitioned(state : State, new_state_name : String)
+
+# Stateに入る時に呼ばれるメソッド
 func _enter():
 _TS_pass
-	
+
+# _process()と同じタイミングで呼ばれるメソッド
 func _update(delta: float):
 _TS_pass
-	
+
+# _physics_process()と同じタイミングで呼ばれるメソッド
 func _physics_update(delta: float):
 _TS_pass
-	
+
+# _input()と同じタイミングで呼ばれるメソッド
 func _input_update(event: InputEvent):
 _TS_pass
 
+# Stateから出る時に呼ばれるメソッド
 func _exit():
 _TS_pass 
